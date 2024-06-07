c6418c60f78d74ce6a94e2212cf58cbbbbb68283
Maarten Behn <maarten.behn@gmail.com>
2024-06-07 20:49:10 +0200

    rebuild

--- home.nix
+++ home.nix
@@ -12,7 +12,7 @@
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
 
-      alias update="cd /home/stroby/nixos/ && git add --all && git commit -m "rebuild" && sudo nixos-rebuild switch --flake ~/nixos/#default --impure && cd -";
+      alias update="cd /home/stroby/nixos/ && git add --all && git commit -m "rebuild" && git push && sudo nixos-rebuild switch --flake ~/nixos/#default --impure && cd -";
       alias clean="sudo nix-collect-garbage --delete-older-than 30d" 
       alias upd=update
       alias ls="exa -l -a";
