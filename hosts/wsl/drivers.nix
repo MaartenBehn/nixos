7c9b5075080d817342f23f2d1ff4da9fdcaccc9e
Maarten Behn <maarten.behn@gmail.com>
2024-12-13 11:24:25 +0100

    wsl network config

--- hosts/wsl/drivers.nix
+++ hosts/wsl/drivers.nix
@@ -9,4 +9,7 @@
   wsl.defaultUser = "nixos";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  wsl.wslConf.network.generateResolvConf = false;
+  networking.nameservers = [ "8.8.8.8" ];
 }
