c27ea16bce6401aaeacc37550577d914fe78dabb
Maarten Behn <maarten.behn@gmail.com>
2025-01-12 00:40:17 +0100

    fix

--- configurations/server/nextcloud.nix
+++ configurations/server/nextcloud.nix
@@ -26,13 +26,7 @@
 
        # Need to manually increment with every major upgrade.
       package = pkgs.nextcloud30;
-
-      # Let NixOS install and configure the database automatically.
-      #database.createLocally = true;
-
-      # Let NixOS install and configure Redis caching automatically.
-      configureRedis = true;
-
+  
       # Increase the maximum file upload size to avoid problems uploading videos.
       maxUploadSize = "16G";
       https = true;
@@ -58,8 +52,6 @@
       };
 
       config = {
-        #dbtype = "pgsql";
-        adminuser = "admin";
         adminpassFile = "/etc/nextcloud-admin-pass";
       };
     };
