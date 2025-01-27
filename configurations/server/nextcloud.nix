2967e5df9552724c72fe03355fa6179fbb162365
Maarten Behn <maarten.behn@gmail.com>
2025-01-28 00:07:33 +0100

    fix

--- configurations/server/nextcloud.nix
+++ configurations/server/nextcloud.nix
@@ -1,5 +1,11 @@
 { pkgs, config, ... }:
 {
+  imports = [
+  "${fetchTarball {
+    url = "https://github.com/onny/nixos-nextcloud-testumgebung/archive/fa6f062830b4bc3cedb9694c1dbf01d5fdf775ac.tar.gz";
+    sha256 = "0gzd0276b8da3ykapgqks2zhsqdv4jjvbv97dsxg0hgrhb74z0fs";}}/nextcloud-extras.nix"
+  ];
+
   environment.etc."nextcloud-admin-pass".text = "Nextcloud+240803";
   environment.etc."nextcloud-stroby-pass".text = "Nextcloud+240803";
 
