129317a813e76f060245acc6c9914dbdda8f8551
Maarten Behn <maarten.behn@gmail.com>
2025-01-28 10:06:26 +0100

    fix

--- configurations/server/filebrowser.nix
+++ configurations/server/filebrowser.nix
@@ -3,7 +3,7 @@
     path = with pkgs; [
       filebrowser
     ];
-    script = "filebrowser --address 0.0.0.0";
+    script = "filebrowser --address 0.0.0.0 --database /home/stroby/filebrowser.db";
     wantedBy = [ "network-online.target" ];
 		after = [ "network.target" ];
     serviceConfig.User = "stroby";
