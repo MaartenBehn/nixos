78c171c5d83cbde52ed8fcb25760678a3a30e284
Maarten Behn <maarten.behn@gmail.com>
2025-01-28 11:38:43 +0100

    fix

--- configurations/server/filebrowser.nix
+++ configurations/server/filebrowser.nix
@@ -29,7 +29,7 @@
         locations."/".proxyPass = "http://127.0.0.1:8080/";
 
         serverAliases = [
-          "www.filebrowser.stroby.duckdns.org
+          "www.filebrowser.stroby.duckdns.org"
         ];
       });
 
