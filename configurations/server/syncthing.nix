f3b77251f594a44a2ef1bc273323395e03760f19
Maarten Behn <maarten.behn@gmail.com>
2025-01-28 11:16:52 +0100

    fix

--- configurations/server/syncthing.nix
+++ configurations/server/syncthing.nix
@@ -7,12 +7,17 @@
       dataDir = "/home/stroby/";    # Default folder for new synced folders
       configDir = "/home/stroby/.config/syncthing";   # Folder for Syncthing's settings and keys
       guiAddress = "0.0.0.0:8384";
+      settings.gui = {
+        user = "stroby";
+        password = "Syncthing+240803";
+      };
     };
   };
 
-  networking.firewall = {
-    allowedTCPPorts = [ 8384 ];
-    allowedUDPPorts = [ 8384 ];
-  };
-
+   # Syncthing ports: 8384 for remote access to GUI
+   # 22000 TCP and/or UDP for sync traffic
+   # 21027/UDP for discovery
+   # source: https://docs.syncthing.net/users/firewall.html
+   networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+   networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }
