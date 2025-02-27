cb647f3a1392897c37f7c10b410b6bb87d84d997
Maarten Behn <maarten.behn@gmail.com>
2025-02-27 18:06:33 +0100

    fix

--- configurations/syncthing.nix
+++ configurations/syncthing.nix
@@ -3,10 +3,12 @@
 {
   services = {
     syncthing = {
-        enable = true;
-        user = "stroby";
-        dataDir = "/home/stroby/";    # Default folder for new synced folders
-        configDir = "/home/stroby/.config/syncthing";   # Folder for Syncthing's settings and keys
+      enable = true;
+      user = "stroby";
+      dataDir = "/home/stroby/";    # Default folder for new synced folders
+      configDir = "/home/stroby/.config/syncthing";   # Folder for Syncthing's settings and keys
+      declarative.overrideDevices = false;
+      declarative.overrideFolders = false;
     };
   };
 }
