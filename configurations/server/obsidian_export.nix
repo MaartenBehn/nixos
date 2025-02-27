5aac9e7e9e2fb03bd9292e647abd18ad3f1483d2
Maarten Behn <maarten.behn@gmail.com>
2025-02-27 16:44:43 +0100

    stuff

--- configurations/server/obsidian_export.nix
+++ configurations/server/obsidian_export.nix
@@ -3,7 +3,7 @@
   services.static-web-server = {
     enable = true;
     root = "/home/stroby/dev/obsidian_export/quartz/public/";
-    listen = "0.0.0.0:8082";
+    listen = "127.0.0.1:8082";
   };
 
   networking.firewall.allowedTCPPorts = [
