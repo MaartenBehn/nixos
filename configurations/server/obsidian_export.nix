a734df7c484374ebd5a46eb777daed4e3c6cf77c
Maarten Behn <maarten.behn@gmail.com>
2025-02-27 17:14:37 +0100

    auto run obsidian updater

--- configurations/server/obsidian_export.nix
+++ configurations/server/obsidian_export.nix
@@ -1,8 +1,19 @@
-{ ... }:
+{ pkgs, username, ... }:
 {
   services.static-web-server = {
     enable = true;
     root = "/home/stroby/dev/obsidian_export/quartz/public/";
     listen = "127.0.0.1:8082";
   };
+
+  systemd.services.obsidian_export-updater = {
+    path = with pkgs; [
+      bash
+    ];
+    script = "nix-shell /home/stroby/dev/obsidian_export/shell.nix --command 'cd /home/stroby/dev/obsidian_export/ && sh rebuild.sh'";
+    startAt = "hourly";  
+    wantedBy = [ "network-online.target" ];
+  };
+  systemd.timers.obsidian_export-updater.timerConfig.RandomizedDelaySec = "15m";
+
 }
