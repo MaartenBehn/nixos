a4f0eabf071629786bfdd96f1aa4b7a2a9126f83
Maarten Behn <maarten.behn@gmail.com>
2025-03-20 22:25:58 +0100

    stuff

--- hosts/asus/home.nix
+++ hosts/asus/home.nix
@@ -4,6 +4,8 @@
     ../../home/base.nix
     ../../home/user.nix
     ../../home/fish.nix
+    ../../home/starship.nix
+    ../../home/nixvim.nix
     ../../home/git.nix
     ../../home/ssh.nix
   ];
