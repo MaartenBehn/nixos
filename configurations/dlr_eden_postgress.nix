26db6fe8bc231afd4d53907b655e7b667c8c64a8
Maarten Behn <maarten.behn@gmail.com>
2025-06-03 10:42:48 +0200

    db

--- configurations/dlr_eden_postgress.nix
+++ configurations/dlr_eden_postgress.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   services.postgresql = {
     enable = true;
-    ensureDatabases = [ "lunadblocal" ];
+    # ensureDatabases = [ "" ];
     enableTCPIP = true;
     port = 5432;
     authentication = pkgs.lib.mkOverride 10 ''
@@ -14,9 +14,9 @@
     host all       all     ::1/128        trust
     '';
     initialScript = pkgs.writeText "backend-initScript" ''
-    CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
-    CREATE DATABASE nixcloud;
-    GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+    CREATE ROLE lunauser WITH LOGIN PASSWORD 'MoondGarden55!' CREATEDB;
+    CREATE DATABASE lunadblocal;
+    GRANT ALL PRIVILEGES ON DATABASE lunadblocal TO lunauser;
     '';
   };
 }
