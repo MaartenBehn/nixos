a8aab7071a420b7be6d9bb0f9eed6da9d0d302fc
Maarten Behn <maarten.behn@gmail.com>
2025-06-30 11:30:21 +0200

    fix

--- configurations/server/nignx.nix
+++ configurations/server/nignx.nix
@@ -66,32 +66,12 @@
       "media.stroby.duckdns.org" = (SSL // {
         locations."/" = {
           proxyPass = "http://127.0.0.1:8096/";
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Protocol $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-
-            proxy_buffering off;
-          '';
-        };
+        
+                 };
 
         locations."/socket" = {
           proxyPass = "http://127.0.0.1:8096/";
-          extraConfig = ''
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Protocol $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-        '';
-        };
+                  };
 
         serverAliases = [
           "www.media.stroby.duckdns.org"
