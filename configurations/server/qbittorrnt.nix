cdf85971440eb32a16f5443c883756d077c5fc5b
Maarten Behn <maarten.behn@gmail.com>
2025-07-04 10:41:59 +0200

    flareslover

--- configurations/server/qbittorrnt.nix
+++ configurations/server/qbittorrnt.nix
@@ -63,5 +63,13 @@
     serviceConfig.User = "stroby";
   };
 
+  # Add systemd service to VPN network namespace
+  systemd.services.flaresolverr.vpnConfinement = {
+    enable = true;
+    vpnNamespace = "wg";
+  };
 
+  services.flaresolverr = {
+    enable = true;
+  };
 }
