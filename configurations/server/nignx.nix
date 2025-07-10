a1f9a56c4582a46e240227fed67406cfe5b5a0ea
Maarten Behn <maarten.behn@gmail.com>
2025-07-10 22:54:03 +0200

    fix

--- configurations/server/nignx.nix
+++ configurations/server/nignx.nix
@@ -77,6 +77,18 @@
       serverAliases = [
         "www.media.stroby.duckdns.org"
       ];
+
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Protocol $scheme;
+        proxy_set_header X-Forwarded-Host $http_host;
+
+        # Disable buffering when the nginx proxy gets very resource heavy upon streaming
+        proxy_buffering off;
+      '';
     });
  
     "qbittorrent.stroby.duckdns.org" = (SSL // {
