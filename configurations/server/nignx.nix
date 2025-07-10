8ce70545931039d3ae5817bb3879e8d9d71b5d59
Maarten Behn <maarten.behn@gmail.com>
2025-07-10 23:05:06 +0200

    fix

--- configurations/server/nignx.nix
+++ configurations/server/nignx.nix
@@ -67,37 +67,28 @@
       locations."/" = {
         proxyPass = "http://127.0.0.1:8096/";
         proxyWebsockets = true;
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          proxy_set_header X-Forwarded-Protocol $scheme;
-          proxy_set_header X-Forwarded-Host $http_host;
-
-          # Disable buffering when the nginx proxy gets very resource heavy upon streaming
-          proxy_buffering off;
-        '';
       };
 
       locations."/socket" = {
         proxyPass = "http://127.0.0.1:8096/";
         proxyWebsockets = true;
-        extraConfig = ''
-          proxy_set_header Upgrade $http_upgrade;
-          proxy_set_header Connection "upgrade";
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          proxy_set_header X-Forwarded-Protocol $scheme;
-          proxy_set_header X-Forwarded-Host $http_host;        
-        '';
       };
 
       serverAliases = [
         "www.media.stroby.duckdns.org"
       ];
+
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Protocol $scheme;
+        proxy_set_header X-Forwarded-Host $http_host;
+
+        # Disable buffering when the nginx proxy gets very resource heavy upon streaming
+        proxy_buffering off; 
+      '';
     });
  
     "qbittorrent.stroby.duckdns.org" = (SSL // {
