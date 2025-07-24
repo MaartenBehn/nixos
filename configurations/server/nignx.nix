e8d000d8639358557253fb80794812d7f12cab48
Maarten Behn <maarten.behn@gmail.com>
2025-07-24 14:10:52 +0200

    test

--- configurations/server/nignx.nix
+++ configurations/server/nignx.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   # SSL 
   security.acme = {
@@ -18,5 +18,30 @@
     recommendedOptimisation = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
+  };
+
+  virtualisation.memorySize = 256;
+  virtualisation.vlans = [ 1 ];
+
+  # To be able to ping containers from the host, it is necessary
+  # to create a macvlan on the host on the VLAN 1 network.
+  networking.macvlans.mv-eth1-host = {
+    interface = "eth1";
+    mode = "bridge";
+  };
+  networking.interfaces.eth1.ipv4.addresses = lib.mkForce [];
+  networking.interfaces.mv-eth1-host = {
+    ipv4.addresses = [ { address = "192.168.178.2"; prefixLength = 24; } ];
+  };
+
+  containers.public-nginx = {
+    autoStart = true;
+    macvlans = [ "eth1" ];
+
+    config = {
+      networking.interfaces.mv-eth1 = {
+        ipv4.addresses = [ { address = "192.168.178.3"; prefixLength = 24; } ];
+      };
+    };
   };
 }
