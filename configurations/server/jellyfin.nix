626306e05435f03752cee2ef6bcba47510b60f88
Maarten Behn <maarten.behn@gmail.com>
2025-07-24 16:00:34 +0200

    fix

--- configurations/server/jellyfin.nix
+++ configurations/server/jellyfin.nix
@@ -1,19 +1,13 @@
-{ domain, ... }:
+{ domains, ... }:
 {
   services.jellyfin.enable = true;
   networking.firewall.allowedUDPPorts = [ 7359 ];
   # Client Discovery (7359/UDP): Allows clients to discover Jellyfin on the local network. A broadcast message to this port will return detailed information about your server that includes name, ip-address and ID.
   
-
   networking.firewall.allowedTCPPorts = [ 8096 ];
-
-  services.nginx.virtualHosts = let
-    SSL = {
-      enableACME = true;
-      forceSSL = true;
-    }; in {
-       
-    "media.${domain}" = (SSL // {
+  services.nginx.virtualHosts = builtins.listToAttrs (builtins.map (domain: {
+    name = "media.${domain}"; 
+    value = {
       locations."/" = {
         proxyPass = "http://127.0.0.1:8096/";
         proxyWebsockets = true;
@@ -57,6 +51,6 @@
       serverAliases = [
         "www.media.${domain}"
       ];
-    });
-  };
+    };
+  }) domains);
 }
