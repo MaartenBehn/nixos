e5c57b3593b76df294b2b7c51c2420ca2aa864cc
Maarten Behn <maarten.behn@gmail.com>
2025-07-24 22:13:58 +0200

    stuff

--- configurations/server/qbittorrnt.nix
+++ configurations/server/qbittorrnt.nix
@@ -46,34 +46,34 @@
     serviceConfig.User = "stroby";
   };
 
-  systemd.services.jackett = {
-    vpnConfinement = {
-      enable = true;
-      vpnNamespace = "wg";
-    };
+  #systemd.services.jackett = {
+    #vpnConfinement = {
+    #  enable = true;
+    #  vpnNamespace = "wg";
+    #};
 
-    path = with pkgs; [
-      curl
-      jackett
-    ];
-    script = "
-    curl curl ipinfo.io;
-    jackett;
-    ";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "multi-user.target" ];
-    serviceConfig.User = "stroby";
-  };
+    #path = with pkgs; [
+      # curl
+      #  jackett
+      #];
+    #script = "
+    #curl curl ipinfo.io;
+    #jackett;
+    #";
+    #wantedBy = [ "multi-user.target" ];
+    #after = [ "multi-user.target" ];
+    #  serviceConfig.User = "stroby";
+  #};
 
   # Add systemd service to VPN network namespace
-  systemd.services.flaresolverr.vpnConfinement = {
-    enable = true;
-    vpnNamespace = "wg";
-  };
+  #systemd.services.flaresolverr.vpnConfinement = {
+  #  enable = true;
+  #  vpnNamespace = "wg";
+  #};
 
-  services.flaresolverr = {
-    enable = true;
-  };
+  #services.flaresolverr = {
+  #  enable = true;
+  #};
 
   services.nginx.virtualHosts = {
 
