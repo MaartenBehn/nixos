ad0dbbbc598e758232e3ab7a94fcdbba87e20af3
Maarten Behn <maarten.behn@gmail.com>
2025-07-24 14:31:13 +0200

    fix

--- configurations/server/gallery_demo.nix
+++ configurations/server/gallery_demo.nix
@@ -20,4 +20,23 @@
       ];
     };
   }) domains);
+
+  containers.public-nginx = {
+    
+
+    services.nginx.virtualHosts = builtins.listToAttrs (builtins.map (domain: {
+      name = "gallery.${domain}"; 
+      value = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8084/"; 
+        };
+
+        serverAliases = [
+          "www.gallery.${domain}"
+        ];
+      };
+    }) domains);
+  };
 }
