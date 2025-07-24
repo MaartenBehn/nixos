9c0c1b4071ce59fe7807b5a4a8b4e09e06443745
Maarten Behn <maarten.behn@gmail.com>
2025-07-24 21:10:23 +0200

    fix

--- home/server/qBittorrent.nix
+++ home/server/qBittorrent.nix
@@ -1,4 +1,4 @@
-{ username, domain, ... }: {
+{ username, ... }: {
   home.file = { 
     "/home/${username}/.local/share/qBittorrent/nova3/engines/jackett.json" = { 
       text = ''
@@ -6,7 +6,7 @@
   "api_key": "mgikmiiem80zmtyecv8xusz9twqxsmzl",
   "thread_count": 20,
   "tracker_first": false,
-  "url": "https://jackett.${domain}"
+  "url": "http://jackett.home"
 }
       ''; 
       executable = false; 
