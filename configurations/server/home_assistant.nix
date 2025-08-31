62c10ee5699b450d1583777455da7509ef5a1d9e
Maarten Behn <maarten.behn@gmail.com>
2025-09-01 00:35:58 +0200

    fix

--- configurations/server/home_assistant.nix
+++ configurations/server/home_assistant.nix
@@ -10,7 +10,17 @@
     config = {
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
+      default_config = {
+        http = {
+          # For extra security set this to only accept connections on localhost if NGINX is on the same machine
+          # Uncommenting this will mean that you can only reach Home Assistant using the proxy, not directly via IP from other clients.
+          server_host = "127.0.0.1";
+          use_x_forwarded_for = true;
+          # You must set the trusted proxy IP address so that Home Assistant will properly accept connections
+          # Set this to your NGINX machine IP, or localhost if hosted on the same machine.
+          trusted_proxies = "127.0.0.1";
+        };
+      };
     };
   };
 
