70e5892465f1ce76212373f5d36a9b9754b5fdec
Maarten Behn <maarten.behn@gmail.com>
2025-09-01 01:35:42 +0200

    fixy

--- configurations/server/mosquitto.nix
+++ configurations/server/mosquitto.nix
@@ -1,13 +1,16 @@
 { local_domain, ... }: {
   services.mosquitto = {
     enable = true;
-    listeners = [
-      {
-        acl = [ "pattern readwrite #" ];
-        omitPasswordAuth = true;
-        settings.allow_anonymous = true;
-      }
-    ];
+    listeners = [{
+      address = "127.0.0.1";
+      port = 1883;
+      users.tasmota = {
+        acl = [
+          "pattern readwrite #"
+        ];
+        password = "mqtt+240803";
+      };  
+    }];  
   };
 
   networking.firewall = {
