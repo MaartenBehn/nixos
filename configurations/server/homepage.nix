b93530f478c2b1ce1f28e11dea8904936bb2da8a
Maarten Behn <maarten.behn@gmail.com>
2025-09-01 02:04:10 +0200

    fix

--- configurations/server/homepage.nix
+++ configurations/server/homepage.nix
@@ -124,7 +124,7 @@
               widget = {
                 type = "homeassistant";
                 url = "http://home.home/";
-                key = "ee7c84e0f9d040b997d8133b14516a8d";
+                key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIwMDFmZGU4OWNjNmE0OTA0OGViMjgyYWU5NGI3YjZkZCIsImlhdCI6MTc1NjY4NTAzMiwiZXhwIjoyMDcyMDQ1MDMyfQ.9UmaK7XB9XmEUDH6CGG92eXOXJYJ_4qC93O9Q-nfDXg";
               };
             };
           }
