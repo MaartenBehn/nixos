83429355bb203f480c72d3ace7b3ce5fa9b224f1
Maarten Behn <maarten.behn@gmail.com>
2025-09-01 22:49:00 +0200

    fix

--- configurations/server/actual_budget.nix
+++ configurations/server/actual_budget.nix
@@ -1,4 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, local_domain, domains, ... }: 
+let
+  configFile = builtins.toJSON(
+  {
+    trustedProxies = [ "127.0.0.1" ];
+  });
+in {
   environment.systemPackages = [
     pkgs.actual-server
   ];
@@ -7,8 +13,25 @@
     path = with pkgs; [
       actual-server
     ];
-      #script = "actual-server";
+    script = "actual-server --config ${configFile}";
     wantedBy = [ "network-online.target" ];
 		after = [ "network.target" ];
   };
+
+  services.nginx.virtualHosts = builtins.listToAttrs (builtins.map (domain: {
+    name = "budget.${domain}"; 
+    value = { 
+      enableACME = domain != local_domain;
+      forceSSL = domain != local_domain;
+
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:5006/";
+        proxyWebsockets = true;
+      };
+
+      serverAliases = [
+        "www.budget.${domain}"
+      ];
+    };
+  }) (domains ++ [ local_domain ]));
 }
