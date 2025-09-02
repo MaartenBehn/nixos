5d425a1d3f9e5ace4d0fbf96512d161c812e3fdf
Maarten Behn <maarten.behn@gmail.com>
2025-09-02 22:06:16 +0200

    fix

--- configurations/server/network.nix
+++ configurations/server/network.nix
@@ -1,11 +1,19 @@
-{ pkgs, ... }:
+{ ... }:
 {
   imports = [
     ./ssh.nix
-    ./static_ip.nix
     ./nignx.nix
   ];
-  
-  networking.firewall.enable = true;
-  networking.networkmanager.enable = true;
+
+  networking = {
+    firewall.enable = true;
+ 
+    networkmanager.enable = true;
+    tempAddresses = "disabled";
+  };
+
+  systemd.network.networks."enp3s0f3u1".ipv6AcceptRAConfig.Token = [
+    "static:::4114:7e5a:3499:9a58"
+    "eui64"
+  ];
 }
--- configurations/server/static_ip.nix
+++
@@ -1,4 +0,0 @@
-{ ... }:
-{
-  networking.tempAddresses = "disabled";
-}
