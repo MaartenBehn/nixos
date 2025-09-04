e5455b04f0ea86cb6f77203e9079b3a7210b99f6
Maarten Behn <maarten.behn@gmail.com>
2025-09-04 23:45:20 +0200

    fix

--- configurations/server/homepage.nix
+++ configurations/server/homepage.nix
@@ -16,9 +16,12 @@ let
   to_var = (s: "HOMEPAGE_FILE_${lib.toUpper (builtins.replaceStrings ["/"] ["_"] s)}");
   to_sops = (s: "homepage/${s}");
   
-  secret_vars = builtins.foldl' 
-    (set: s: builtins.listToAttrs [{ name = s; value = set; }]) 
-    (builtins.map (s: lib.strings.splitString "/" s) secrets);
+  secret_vars = (builtins.map (s: 
+    builtins.foldl' 
+      (set: s: builtins.listToAttrs [{ name = s; value = set; }])
+      (to_var s)
+      (lib.lists.reverseList (lib.strings.splitString "/" s)) 
+    ) secrets);
 
   secret_environment = builtins.listToAttrs (builtins.map (s: 
     {
