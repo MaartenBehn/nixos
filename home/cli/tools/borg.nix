198589909c038dee3e4f1122716b32c8a788858f
Maarten Behn <maarten.behn@gmail.com>
2025-09-10 00:55:11 +0200

    mount notes proxy

--- home/cli/tools/borg.nix
+++ home/cli/tools/borg.nix
@@ -5,10 +5,17 @@ let
     borgfs -p ssh://Stroby@192.168.178.39/volume1/BackUp/asus_server ~/NotesBackupFritzBehns
   '');
 
+  borg_mount_proxy_notes = pkgs.writeShellScriptBin "borg_mount_proxy_notes" (''
+    mkdir -p +/NotesBackupProxy
+    borgfs -p ssh://root@138.199.203.38/backup/notes ~/NotesBackupProxy
+  '');
+
+
 in {
   home.packages = (with pkgs; [
     borgbackup
     borg_mount_fritz_behn_notes
+    borg_mount_proxy_notes
   ]);
 
 }
