dcfea4cada314f3dbede2b15381f18b5b4d6cc12
Maarten Behn <maarten.behn@gmail.com>
2025-09-14 17:13:14 +0200

    stuff

--- configurations/server/borg.nix
+++ configurations/server/borg.nix
@@ -26,6 +26,7 @@
     };
   };
 
+  # Notes
   services.borgbackup.jobs.fritz_behns_notes = {
     paths = "/home/${username}/Notes";
     encryption.mode = "none";
@@ -43,6 +44,23 @@
     repo = "ssh://root@138.199.203.38/backup/notes";
     compression = "auto,zstd";
     startAt = "daily";
+    user = "stroby";
+  };
+
+  systemd.services.borgbackup-job-fritz_behns_shibari_study = {
+    vpnConfinement = {
+      enable = true;
+      vpnNamespace = "fritz";
+    };
+  };
+
+  services.borgbackup.jobs.fritz_behns_shibari_study = {
+    paths = "/media/videos/ShibariStudy";
+    encryption.mode = "none";
+    environment.BORG_RSH = "ssh -i /home/${username}/.ssh/id_ed25519";
+    repo = "ssh://Stroby@192.168.178.39/volume1/BackUp/asus_server/study";
+    compression = "auto,zstd";
+    startAt = "month";
     user = "stroby";
   };
 }
