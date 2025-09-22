cc84d1558acd5dcdd2abc8868d2aa600b90b71af
Maarten Behn <maarten.behn@gmail.com>
2025-09-22 23:17:05 +0200

    fix

--- configurations/server/nextcloud.nix
+++ configurations/server/nextcloud.nix
@@ -9,7 +9,7 @@ in {
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud31;
+    package = pkgs.nextcloud30;
 
     database.createLocally = true;
     configureRedis = true;
