53eda3a43fef7ff53e68b82ef0a394c52def2646
Maarten Behn <maarten.behn@gmail.com>
2025-09-22 14:16:52 +0200

    fix

--- configurations/server/borg.nix
+++ configurations/server/borg.nix
@@ -8,11 +8,12 @@
     isNormalUser = true;
   };
 
+  # borg users ~/.ssh/id_ed25519.pub must be added to authorized_key in DiskStation
   # https://blog.aaronlenoir.com/2018/05/06/ssh-into-synology-nas-with-ssh-key/
   vpnNamespaces.fritz = {
     enable = true;
-    wireguardConfigFile = config.sops.secrets."wireguard/fritz_behns_stroby.conf".path;
-    #wireguardConfigFile = "/home/stroby/fritz_behns_asus_borg.conf";
+    #wireguardConfigFile = config.sops.secrets."wireguard/fritz_behns_stroby.conf".path;
+    wireguardConfigFile = "/home/borg/fritz_behns_asus_borg.conf";
     namespaceAddress = "192.168.16.1";
     bridgeAddress = "192.168.16.5";
  
