651dced9bdd65663965d45fdf6f14b9e606ddf20
Maarten Behn <maarten.behn@gmail.com>
2025-09-22 23:30:40 +0200

    stuff

--- configurations/server/nextcloud.nix
+++ configurations/server/nextcloud.nix
@@ -9,7 +9,7 @@ in {
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
 
     database.createLocally = true;
     configureRedis = true;
