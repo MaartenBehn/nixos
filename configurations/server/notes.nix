c72171301141e84bb1224ef5ba4598da3d459eb5
Maarten Behn <maarten.behn@gmail.com>
2025-09-22 14:06:10 +0200

    stuff

--- configurations/server/notes.nix
+++ configurations/server/notes.nix
@@ -16,24 +16,25 @@
       curl http://ipecho.net/plain; echo
     ''; 
   };
+  users.groups.notes.members = [ "borg" ];
 
   services.borgbackup.jobs.fritz_behns_notes = {
     paths = "/notes";
     encryption.mode = "none";
-    environment.BORG_RSH = "ssh -i /home/${username}/.ssh/id_ed25519";
+    environment.BORG_RSH = "ssh -i /home/borg/.ssh/id_ed25519";
     repo = "ssh://Stroby@192.168.178.39/volume1/BackUp/asus_server/notes";
     compression = "auto,zstd";
     startAt = "*-*-* 04:15";
-    user = "stroby";
+    user = "borg";
   };
 
   services.borgbackup.jobs.proxy_notes = {
     paths = "/notes";
     encryption.mode = "none";
-    environment.BORG_RSH = "ssh -i /home/${username}/.ssh/id_ed25519";
+    environment.BORG_RSH = "ssh -i /home/borg/.ssh/id_ed25519";
     repo = "ssh://root@138.199.203.38/backup/notes";
     compression = "auto,zstd";
     startAt = "*-*-* 04:10";
-    user = "stroby";
+    user = "borg";
   };
 }
