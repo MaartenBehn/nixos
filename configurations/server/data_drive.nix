35c93800f90236f06ca66a3596ac1a8f8484a953
Maarten Behn <maarten.behn@gmail.com>
2025-09-24 23:07:23 +0200

    fix

--- configurations/server/data_drive.nix
+++ configurations/server/data_drive.nix
@@ -2,17 +2,12 @@
   
   fileSystems = {
 
-    "/media/video_data" = {
-      device = "/dev/disk/by-uuid/3d984e59-b783-45f6-b58c-ab2d6a03126d/media/video";
-      fsType = "ext4";
-      #options = [ # If you don't have this options attribute, it'll default to "defaults" 
-      # boot options for fstab. Search up fstab mount options you can use
-      #"users" # Allows any user to mount and unmount
-      #"nofail" # Prevent system from failing if this drive doesn't mount
-      #];
-    };
+    #"/media/video_data" = {
+    #  device = "/dev/disk/by-uuid/3d984e59-b783-45f6-b58c-ab2d6a03126d/media/video";
+    #  fsType = "ext4";
+    #};
 
-    "/mnt/exampleDrive" = {
+    "/mnt/Data" = {
       device = "/dev/disk/by-uuid/3d984e59-b783-45f6-b58c-ab2d6a03126d";
       fsType = "ext4";
     };
