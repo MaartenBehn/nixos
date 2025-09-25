b3bc9393c3ac10bc4b3a3712503040841e9999d4
Maarten Behn <maarten.behn@gmail.com>
2025-09-25 20:10:57 +0200

    stuff

--- configurations/server/syncthing.nix
+++ configurations/server/syncthing.nix
@@ -3,7 +3,7 @@
   services = {
     syncthing = {
       enable = true;
-      user = "stroby";
+      user = "syncthing";
       dataDir = "/home/stroby/";    # Default folder for new synced folders
       configDir = "/home/stroby/.config/syncthing";   # Folder for Syncthing's settings and keys
       guiAddress = "0.0.0.0:8384";
