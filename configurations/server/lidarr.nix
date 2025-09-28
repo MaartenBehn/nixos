d405ecd676711e1aec03ba232abd5058752197ea
Maarten Behn <maarten.behn@gmail.com>
2025-09-28 10:57:15 +0200

    lidarr plugin branch

--- configurations/server/lidarr.nix
+++ configurations/server/lidarr.nix
@@ -1,10 +1,30 @@
-{ pkgs-unstable, local_domain, ... }: {
+{ pkgs-unstable, local_domain, ... }: 
+let
+
+plugin_branch = pkgs-unstable.lidarr.overrideAttrs (old: {
+  version = "plugins"; # usually harmless to omit
+  src = pkgs-unstable.fetchFromGitHub {
+    owner = "Lidarr";
+    repo = "Lidarr";
+    rev = "6e5f2f6f844daadc3a9abf4955b984da87b77dde";
+    # If you don't know the hash, the first time, set:
+    # hash = "";
+    # then nix will fail the build with such an error message:
+    # hash mismatch in fixed-output derivation '/nix/store/m1ga09c0z1a6n7rj8ky3s31dpgalsn0n-source':
+    # specified: sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
+    # got:    sha256-173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4
+      #hash = "sha256-173gxk0ymiw94glyjzjizp8bv8g72gwkjhacigd1an09jshdrjb4";
+  };    
+});
+
+in {
   users.groups.media.members = [ "lidarr" ];
 
   services.lidarr = { 
     enable = true;
     openFirewall = false;
-    package = pkgs-unstable.lidarr;
+    ##package = pkgs-unstable.lidarr;
+    package = plugin_branch;
   };
 
   systemd.services.lidarr.vpnConfinement = {
