aebf211f3f5d8dfcc58b0895368224a008c4f522
Maarten Behn <maarten.behn@gmail.com>
2025-10-04 23:26:10 +0200

    fix

--- configurations/server/slskd.nix
+++ configurations/server/slskd.nix
@@ -1,8 +1,20 @@
-{ local_domain, lib, ... }: {
+{ pkgs, local_domain, lib, ... }: 
+let 
+env_file = pkgs.writeTextFile {
+    name = "slskd_env";
+    text = ''
+#SLSKD_SLSK_USERNAME = ;
+#SLSKD_SLSK_PASSWORD = ;
+SLSKD_USERNAME = Stroby241;
+SLSKD_PASSWORD = SoulSeek+240803;
+    '';
+  };
+in {
   services.slskd = { 
     enable = true;
     openFirewall = false;
     domain = "slskd.${local_domain}";
+    environmentFile = env_file;
   };
   
   systemd.services.slskd = {
