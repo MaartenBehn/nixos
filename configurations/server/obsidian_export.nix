9fc65ba78abd54152ddc9d2a8e604c0395b2bf15
Maarten Behn <maarten.behn@gmail.com>
2025-10-16 12:54:18 +0200

    fix

--- configurations/server/obsidian_export.nix
+++ configurations/server/obsidian_export.nix
@@ -17,6 +17,8 @@ let
   '';
   update = pkgs.writeShellScriptBin "update" ''
     cd /srv/obsidian_export 
+    git pull -f
+
     export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
     export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
 
