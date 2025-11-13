4e6744db547605543c453083df6380e7c852790f
Maarten Behn <maarten.behn@gmail.com>
2025-11-13 11:01:49 +0100

    fix

--- configurations/server/mail.nix
+++ configurations/server/mail.nix
@@ -4,7 +4,7 @@ let
   main_domain = "stroby.ipv64.de";
 in {
   imports = [
-    inputs.mailserver.nixosModules.default
+    inputs.mailserver.mailserverModule
   ];
 
   sops.secrets."mail/asus/hash" = { owner = "mailserver"; };
