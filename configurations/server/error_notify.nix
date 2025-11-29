5de3e73e2e6e2dc1cd3185b0804bb804ab195b81
Maarten Behn <maarten.behn@gmail.com>
2025-11-29 16:28:42 +0100

    fix

--- configurations/server/error_notify.nix
+++ configurations/server/error_notify.nix
@@ -1,35 +1,21 @@
 { pkgs, ... }: 
 let
   unit_status = pkgs.writeShellScriptBin "unit_status" ''
-    curl http://localhost:8090/status 
-    -d "
-    Status report for unit: $1
-    blas 
-    bjsabf
-    nklsafn
-    nkasjf
-    nsaf"
-    
-    #UNIT=$1
+    UNIT=$1
 
-    #EXTRA=""
-    #for e in "''${@:2}"; do
-    #EXTRA+="$e"$'\n'
-    #done
+    EXTRA=""
+    for e in "''${@:2}"; do
+      EXTRA+="$e"$'\n'
+    done
 
-    #UNITSTATUS=$(systemctl status $UNIT)
+    UNITSTATUS=$(systemctl status $UNIT)
 
-    #MSG=$(cat <<EOF
-    #Status report for unit: $UNIT
-    #$EXTRA
+    curl http://localhost:8090/status -d "
+    Status report for unit: $UNIT
+    $EXTRA
 
-    #$UNITSTATUS
-    #EOF
-    #)
-
-    #echo $MSG
-
-    #curl -d "fail" http://localhost:8090/status
+    $UNITSTATUS
+    "    
     '';
 in {
   systemd.services."unit-status@" = {
@@ -43,6 +29,6 @@ in {
 
   systemd.services."allways-fails" = {
     script = "exit -1";
-    onFailure = [ "unit-status@%n.service" ];
+    onFailure = [ "unit-status@%n" ];
   };
 }
