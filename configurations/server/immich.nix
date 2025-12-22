0aa1903b57129609f7563574144c30e938a916e9
Maarten Behn <maarten.behn@gmail.com>
2025-12-22 01:48:56 +0100

    fix

--- configurations/server/immich.nix
+++ configurations/server/immich.nix
@@ -42,6 +42,7 @@ in {
   };
 
   services.borgbackup.jobs.fritz_behns_immich = default_borg_settings // {
+    user = "immich";
     group = "immich";
     paths = "/var/lib/immich";
     repo = "ssh://Stroby@192.168.178.39/volume1/BackUp/asus_server/immich";
