1e12653f2812a0c31f2adf52f443541e7458ab22
Maarten Behn <maarten.behn@gmail.com>
2025-12-27 12:03:49 +0100

    stuff

--- configurations/server/mail.nix
+++ configurations/server/mail.nix
@@ -38,9 +38,10 @@ in {
   security.acme = {
     acceptTerms = true;
     certs."mx.stroby.org" = {
-      dnsProvider = "Cloudflare";
+      dnsProvider = "cloudflare";
       environmentFile = "${config.sops.templates."maddy_acme.env".path}";
       group = config.services.maddy.group;
+      postRun = "echo $CLOUDFLARE_DNS_API_TOKEN";
     };
   };
 
