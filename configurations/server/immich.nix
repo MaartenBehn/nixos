5431e94e9892f514a923aa4cce5a1d2a11745e68
Maarten Behn <maarten.behn@gmail.com>
2026-01-05 00:30:16 +0100

    fix

--- configurations/server/immich.nix
+++ configurations/server/immich.nix
@@ -30,15 +30,18 @@ in {
   hardware.graphics.enable = true;
 
   web_services."immich" = {
-    proxyPass = "http://[::1]:${toString config.services.immich.port}";
-    proxyWebsockets = true;
-    recommendedProxySettings = true;
-    extraConfig = ''
+    domains = "all";
+    loc = {
+      proxyPass = "http://[::1]:${toString config.services.immich.port}";
+      proxyWebsockets = true;
+      recommendedProxySettings = true;
+      extraConfig = ''
       client_max_body_size 50000M;
       proxy_read_timeout   600s;
       proxy_send_timeout   600s;
       send_timeout         600s;
-    '';      
+      '';    
+    };
   };
 
   systemd.services.borgbackup-job-fritz_behns_immich = {
--- configurations/server/nignx_web_service.nix
+++ configurations/server/nignx_web_service.nix
@@ -2,24 +2,40 @@
 {
   options = {
     web_services = lib.mkOption {
-      type = lib.types.attrsOf lib.types.attrs;
-      description = "Get passed into services.nginx.virtualHosts.locations.\"/\"";
+      type = lib.types.attrsOf (lib.types.submodule {
+        options = {
+          domains = lib.mkOption {
+            type = lib.types.enum [ "local" "public" "all" ];
+            description = "On local, public or all domains";
+            default = "all";
+          };
+ 
+          loc = lib.mkOption {
+            type = lib.types.attrs;
+            description = "Get passed into services.nginx.virtualHosts.locations.\"/\"";
+          };
+        };
+      });
     };
   };
 
-  config = {
+  config = let
+      get_webservice = sub_domain: builtins.getAttr sub_domain config.web_services;
+      get_domains = sub_domain: if (get_webservice sub_domain).domains == "local" then [ "local" ] else (
+        if (get_webservice sub_domain).domains == "public" then config.domains.public else config.domains.all);
+    in {
     services.nginx.virtualHosts = builtins.listToAttrs (lib.lists.flatten (builtins.map (sub_domain: 
       builtins.map (domain: {
         name = "${sub_domain}.${domain}"; 
         value = {
           enableACME = domain != "local";
           forceSSL = domain != "local";
-          locations."/" = builtins.getAttr sub_domain config.web_services; 
+          locations."/" = (get_webservice sub_domain).loc; 
           serverAliases = [
             "www.${sub_domain}.${domain}"
           ];
         };
-      }) config.domains.all)
+      }) (get_domains sub_domain))
       (builtins.attrNames config.web_services)));
   };
 }
