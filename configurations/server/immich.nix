35956171db8e4cf4435c9a45c2f43f0c1e035f6b
Maarten Behn <maarten.behn@gmail.com>
2026-01-04 20:12:51 +0100

    fix

--- configurations/server/immich.nix
+++ configurations/server/immich.nix
@@ -34,14 +34,14 @@ in {
       sub_domain = "immich";
       config = {
         proxyPass = "http://[::1]:${toString config.services.immich.port}";
-          proxyWebsockets = true;
-          recommendedProxySettings = true;
-          extraConfig = ''
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+        extraConfig = ''
             client_max_body_size 50000M;
             proxy_read_timeout   600s;
             proxy_send_timeout   600s;
             send_timeout         600s;
-          '';      
+        '';      
       };
     }
   ];
