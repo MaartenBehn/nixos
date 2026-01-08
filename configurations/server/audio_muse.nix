8a88a09a478958c6e24d84762861c47dc049dcce
Maarten Behn <maarten.behn@gmail.com>
2026-01-08 02:40:11 +0100

    fix

--- configurations/server/audio_muse.nix
+++ configurations/server/audio_muse.nix
@@ -147,7 +147,7 @@ in {
     "audio_muse_server" = {
       domains = "local";
       loc = {
-        proxyPass = "http://172.0.0.1:8081";
+        proxyPass = "http://172.0.0.1:8081/";
       };
     };
   };
