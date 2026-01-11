7ea52088c4f5ae7d7b22e2286cd2de89aba53ab2
Maarten Behn <maarten.behn@gmail.com>
2026-01-11 02:55:29 +0100

    fix

--- home/apps/beets/beets-plugin-mood-writer.nix
+++ home/apps/beets/beets-plugin-mood-writer.nix
@@ -7,7 +7,7 @@ python3Packages.buildPythonApplication rec {
     owner = "MaartenBehn";
     repo = "beets-mood";
     rev = "1f098666b8682d227232f2ba8ee7744bcfca9de1";
-    hash = "sha256-+6Mq8mR/tF1Z+GyooMz9fWGV57bFhbHXBVBDkHpUMDA="; # build and get the hash from the error
+    hash = "sha256-u5nv77NIVEuO/DU1pDL3CcYa1OURkcjsKv+4rrpMLfI=";
   };
 
   nativeBuildInputs = [
