930859a6e0ea77a819d4c5c0019f4795f8598f2d
Maarten Behn <maarten.behn@gmail.com>
2026-01-11 03:37:21 +0100

    fix

--- home/apps/beets/beets-plugin-mood-writer.nix
+++ home/apps/beets/beets-plugin-mood-writer.nix
@@ -7,7 +7,7 @@ python3Packages.buildPythonApplication rec {
     owner = "MaartenBehn";
     repo = "beets-mood";
     rev = "edeae040c23b5b794becdfc04274c0790c27c1d3";
-    hash = "";
+    hash = "sha256-5kAl5KLZzxY3ypu+isWpYrbxF0QwghjClXR2wH5L7cs=";
   };
 
   nativeBuildInputs = [
