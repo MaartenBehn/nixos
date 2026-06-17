93f9efe647d3a93d09134df39d834e801c62394d
Maarten Behn <maarten.behn@gmail.com>
2026-06-17 14:54:54 +0200

    fix

--- modules/server/mullvad.nix
+++ modules/server/mullvad.nix
@@ -9,8 +9,8 @@
 
     vpnNamespaces.mullvad = {
       enable = true;
-      wireguardConfigFile = config.sops.secrets."wireguard/mullvad.conf".path;
-      #wireguardConfigFile = /home/stroby/.config/wireguard/mullvad.conf;
+      #wireguardConfigFile = config.sops.secrets."wireguard/mullvad.conf".path;
+      wireguardConfigFile = /home/stroby/.config/wireguard/mullvad.conf;
 
       accessibleFrom = [
         "192.168.0.0/24"
