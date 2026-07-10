a59d04e1c9a13258b06c21e81b02ce622ef14efd
Maarten Behn <maarten.behn@gmail.com>
2026-07-11 00:16:36 +0200

    fix

--- modules/cli/man.nix
+++ modules/cli/man.nix
@@ -7,7 +7,9 @@
   flake.modules.homeManager.base = {
     programs.man.generateCaches = false;
   };
-  
+
+  flake.modules.nixos.cli-full = { pkgs, ... }: {
+  };  
 
   flake.modules.homeManager.cli-full = { pkgs, ... }: {
     home.packages = (with pkgs; [    
