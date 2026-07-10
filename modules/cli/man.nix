8f5d93185b9408835fbcb7d1cd10b5bef1b9fe7d
Maarten Behn <maarten.behn@gmail.com>
2026-07-11 00:15:02 +0200

    remove man

--- modules/cli/man.nix
+++ modules/cli/man.nix
@@ -1,13 +1,18 @@
 {
-  flake.modules.nixos.cli-full = {
-    documentation.nixos.enable = true;
-    documentation.man.generateCaches = true;
+  flake.modules.nixos.base = {
+    documentation.nixos.enable = false;
+    documentation.man.generateCaches = false;
   };
+
+  flake.modules.homeManager.base = {
+    programs.man.generateCaches = false;
+  };
+  
 
   flake.modules.homeManager.cli-full = { pkgs, ... }: {
     home.packages = (with pkgs; [    
-      tldr                              # Better man
-      man-pages       
+      #tldr                              # Better man
+      #man-pages       
     ]);
   };
 }
