82396c2ce271cbf97114d81710141161650d7a54
Maarten Behn <maarten.behn@gmail.com>
2026-07-16 18:55:41 +0200

    test

--- modules/services/wireguard.nix
+++ modules/services/wireguard.nix
@@ -8,23 +8,45 @@
       sops.secrets."wireguard/private_local.conf" = { owner = "stroby"; };
       sops.secrets."wireguard/dont_panic.conf" = { owner = "stroby"; };
       sops.secrets."wireguard/fritz_behns_stroby.conf" = { owner = "stroby"; };
+      
+      sops.secrets."wireguard/private/laptop/private_key" = { owner = "stroby"; };
+      sops.secrets."wireguard/private_local/laptop/private_key" = { owner = "stroby"; };
 
-      networking.wg-quick.interfaces = {
+    networking.wg-quick.interfaces = {
 
-        private = {  
-          autostart = false;
-          configFile = config.sops.secrets."wireguard/private.conf".path;
-        };
+      private = {  
+        privateKeyFile = config.sops.secrets."wireguard/private/laptop/private_key".path;
+        address = "10.1.0.3/24";
+        dns = "10.1.0.2";
 
-        private_local = {  
-          autostart = false;
-          configFile = config.sops.secrets."wireguard/private_local.conf".path;
-        };
+        peers = [
+          {
+            publicKey = "y/Up4Ps6jIdZHzOL2LDYnkZB3JL03MZtZmGLZXESr1U=";
+            endpoint = "138.199.203.38:51821";
+            allowedIPs = [ "10.1.0.0/24" ]; 
+            persistentKeepalive = 25;
+          }
+        ];
 
-        dont_panic = {  
-          autostart = false;
-          configFile = config.sops.secrets."wireguard/dont_panic.conf".path;
-        };
+        autostart = false;
+      };
+
+      private_local = {  
+        privateKeyFile = config.sops.secrets."wireguard/private/laptop/private_key".path;
+        address = "10.2.0.2/24";
+        dns = "10.2.0.1";
+
+        peers = [
+          {
+            publicKey = "y/Up4Ps6jIdZHzOL2LDYnkZB3JL03MZtZmGLZXESr1U=";
+            endpoint = "192.168.0.117:51821";
+            allowedIPs = [ "10.2.0.0/24" "10.1.0.0/24" ];
+            persistentKeepalive = 25;
+          }
+        ];
+
+        autostart = false;
+      };
 
         fritz_behns = {      
           autostart = false;
