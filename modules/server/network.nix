6264f017117d370ea08abd008773a0b32d761498
Maarten Behn <maarten.behn@gmail.com>
2026-07-22 23:47:32 +0200

    fix

--- modules/server/network.nix
+++ modules/server/network.nix
@@ -3,15 +3,17 @@
     networking = {
       firewall.enable = true;
    
-      networkmanager.enable = false;
+      networkmanager.enable = true;
+      /*
       tempAddresses = "disabled";
 
       interfaces.enp3s0f3u1 = {
         macAddress = "02:11:22:33:44:55";
       };
 
-      nameservers = [ "1.1.1.1" "1.0.0.1" ];
       dhcpcd.extraConfig = '' nohook resolv.conf '';
+      */
+      nameservers = [ "1.1.1.1" "1.0.0.1" ];
     };
   };
 }
