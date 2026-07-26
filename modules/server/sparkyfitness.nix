4e1310ab34e4b9124747c7b6d1488f03190d924c
Maarten Behn <maarten.behn@gmail.com>
2026-07-27 01:11:17 +0200

    fix

--- modules/server/sparkyfitness.nix
+++ modules/server/sparkyfitness.nix
@@ -24,14 +24,61 @@
       enableACME = true;
       forceSSL = true;
 
-      locations."/".extraConfig = ''
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        add_header X-Content-Type-Options "nosniff";
-        proxy_set_header X-Forwarded-Ssl on;
-      '';
+      locations = {
+        "/".extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          add_header X-Content-Type-Options "nosniff";
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+
+        "/assets/".extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          add_header X-Content-Type-Options "nosniff";
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+
+        "^~ /api/".extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          add_header X-Content-Type-Options "nosniff";
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+
+        "/health-data".extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          add_header X-Content-Type-Options "nosniff";
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+
+        "^~ /uploads/".extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          add_header X-Content-Type-Options "nosniff";
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+
+        "^~ /mcp".extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          add_header X-Content-Type-Options "nosniff";
+          proxy_set_header X-Forwarded-Ssl on;
+        '';
+      };
     };
   };
 }
