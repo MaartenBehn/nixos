4b9683cb2b48cca47318bcbf377dbfb082af4e0f
Maarten Behn <maarten.behn@gmail.com>
2026-07-27 01:30:15 +0200

    fix

--- modules/server/sparkyfitness.nix
+++ modules/server/sparkyfitness.nix
@@ -29,36 +29,108 @@
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
           proxy_set_header Connection "Close";
+
+          client_body_buffer_size 128k;
+
+          proxy_connect_timeout 90;
+          proxy_send_timeout 90;
+          proxy_read_timeout 90;
+          proxy_buffers 4 32k;
+          client_max_body_size 0;
+          proxy_pass_header Authorization;
+
+          proxy_redirect off;
+          proxy_http_version 1.1;
         '';
 
         "/assets/".extraConfig = ''
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
           proxy_set_header Connection "Close";
+
+          client_body_buffer_size 128k;
+
+          proxy_connect_timeout 90;
+          proxy_send_timeout 90;
+          proxy_read_timeout 90;
+          proxy_buffers 4 32k;
+          client_max_body_size 0;
+          proxy_pass_header Authorization;
+
+          proxy_redirect off;
+          proxy_http_version 1.1;
         '';
 
         "^~ /api/".extraConfig = ''
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
           proxy_set_header Connection "Close";
+
+          client_body_buffer_size 128k;
+
+          proxy_connect_timeout 90;
+          proxy_send_timeout 90;
+          proxy_read_timeout 90;
+          proxy_buffers 4 32k;
+          client_max_body_size 0;
+          proxy_pass_header Authorization;
+
+          proxy_redirect off;
+          proxy_http_version 1.1;
         '';
 
         "/health-data".extraConfig = ''
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
           proxy_set_header Connection "Close";
+
+          client_body_buffer_size 128k;
+
+          proxy_connect_timeout 90;
+          proxy_send_timeout 90;
+          proxy_read_timeout 90;
+          proxy_buffers 4 32k;
+          client_max_body_size 0;
+          proxy_pass_header Authorization;
+
+          proxy_redirect off;
+          proxy_http_version 1.1;
         '';
 
         "^~ /uploads/".extraConfig = ''
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
           proxy_set_header Connection "Close";
+
+          client_body_buffer_size 128k;
+
+          proxy_connect_timeout 90;
+          proxy_send_timeout 90;
+          proxy_read_timeout 90;
+          proxy_buffers 4 32k;
+          client_max_body_size 0;
+          proxy_pass_header Authorization;
+
+          proxy_redirect off;
+          proxy_http_version 1.1;
         '';
 
         "^~ /mcp".extraConfig = ''
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
           proxy_set_header Connection "Close";
+
+          client_body_buffer_size 128k;
+
+          proxy_connect_timeout 90;
+          proxy_send_timeout 90;
+          proxy_read_timeout 90;
+          proxy_buffers 4 32k;
+          client_max_body_size 0;
+          proxy_pass_header Authorization;
+
+          proxy_redirect off;
+          proxy_http_version 1.1;
         '';
       };
     };
