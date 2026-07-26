19ff006d64fc15f0868d3ba8f9717a7c03d7fd3f
Maarten Behn <maarten.behn@gmail.com>
2026-07-27 01:21:18 +0200

    fix

--- modules/server/sparkyfitness.nix
+++ modules/server/sparkyfitness.nix
@@ -26,55 +26,31 @@
 
       locations = {
         "/".extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
         '';
 
         "/assets/".extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
         '';
 
         "^~ /api/".extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
         '';
 
         "/health-data".extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
         '';
 
         "^~ /uploads/".extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
         '';
 
         "^~ /mcp".extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
           add_header X-Content-Type-Options "nosniff";
           proxy_set_header X-Forwarded-Ssl on;
         '';
