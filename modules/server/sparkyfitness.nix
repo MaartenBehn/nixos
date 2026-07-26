a9da8fae4d3b1257586c3378721cbf07a8df27d4
Maarten Behn <maarten.behn@gmail.com>
2026-07-27 00:45:33 +0200

    fix

--- modules/server/sparkyfitness.nix
+++ modules/server/sparkyfitness.nix
@@ -24,16 +24,14 @@
       enableACME = true;
       forceSSL = true;
 
-      locations."/" = {
-        extraConfig = ''
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-          add_header X-Content-Type-Options "nosniff";
-          proxy_set_header X-Forwarded-Ssl on;
-        '';
-      };
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        add_header X-Content-Type-Options "nosniff";
+        proxy_set_header X-Forwarded-Ssl on;
+      '';
     };
   };
 }
