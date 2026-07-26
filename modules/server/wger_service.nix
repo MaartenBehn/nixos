2d8c33fcf54d1870a937418c69ea1b83f9c6f858
Maarten Behn <maarten.behn@gmail.com>
2026-07-26 15:10:34 +0200

    fix

--- modules/server/wger_service.nix
+++ modules/server/wger_service.nix
@@ -101,7 +101,7 @@
         cd "${wgerSrc}"
 
         # Sync Python environment using uv
-        uv sync --group docker --no-managed-python --python ${pkgs.python311}/bin/python
+        uv sync --group docker --no-managed-python --python ${pkgs.python312}/bin/python
 
         # Activate virtualenv
         source .venv/bin/activate
