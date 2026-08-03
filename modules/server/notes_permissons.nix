80ee6e30ed10d60989db452b61c7ee384044c869
Maarten Behn <maarten.behn@gmail.com>
2026-08-03 16:20:24 +0200

    fix

--- modules/server/notes_permissons.nix
+++ modules/server/notes_permissons.nix
@@ -4,11 +4,11 @@
       # Recursively set ownership (stroby:media) and mode 2750
       "Z /notes 2750 syncthing notes - -"
 
-      # Set Access ACL recursively so the media group gets rwx on all current items
-      "A+ /notes - - - - group:notes:rwx"
+      # Set Access ACL recursively so the media group gets r-x on all current items
+      "A+ /notes - - - - group:notes:r-x"
 
-      # Set Default ACL so all future files/directories inherit group:media:rwx permissions
-      "a+ /notes - - - - group:notes:rwx"
+      # Set Default ACL so all future files/directories inherit group:media:r-x permissions
+      "a+ /notes - - - - group:notes:r-x"
     ];
   };
 }
