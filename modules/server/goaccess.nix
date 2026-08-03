0881904948e0b99cf982f57dbe4456e37e4467f4
Maarten Behn <maarten.behn@gmail.com>
2026-08-04 01:01:11 +0200

    goaccess

---
+++ modules/server/goaccess.nix
@@ -0,0 +1,51 @@
+{
+  flake.modules.nixos.server = { pkgs, ... }: {
+    environment.systemPackages = [ pkgs.goaccess ];
+
+    systemd.services.goaccess = {
+      description = "GoAccess Real-time Web Log Analyzer";
+      after = [ "network.target" "nginx.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = ''
+          ${pkgs.goaccess}/bin/goaccess /var/log/nginx/access.log \
+          -o /var/lib/goaccess/index.html \
+          --log-format=COMBINED \
+          --real-time-html \
+          --ws-url=ws://stats.local.yourdomain:80/goaccess-ws \
+          --port=7890
+        '';
+        Restart = "always";
+        RestartSec = "5s";
+        StateDirectory = "goaccess";
+        User = "root"; # Needed to access Nginx access logs
+      };
+    };
+
+    services.nginx.appendHttpConfig = ''
+    access_log /var/log/nginx/access.log combined;
+    '';
+
+    web_services.stats = {
+      domains = "local";
+
+      root = {
+        alias = "/var/lib/goaccess/";
+        index = "index.html";
+      };
+
+      locations = {
+        "/goaccess-ws" = {
+          proxyPass = "http://127.0.0.1:7890";
+          proxyHttpVersion = "1.1";
+          extraConfig = ''
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "Upgrade";
+          '';
+        };
+      };
+    };
+  };
+}
