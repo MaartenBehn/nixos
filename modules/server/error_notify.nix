1df16920a1e9da50a4faa92ca86f4cbdab49b156
Maarten Behn <maarten.behn@gmail.com>
2026-08-07 15:30:33 +0200

    fix

--- modules/server/error_notify.nix
+++ modules/server/error_notify.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.nixos.server = { pkgs, ... }: 
+  flake.modules.nixos.server = { pkgs, lib, ... }: 
   let
     unit_status = pkgs.writeShellScriptBin "unit_status" ''
       UNIT=$1
@@ -19,14 +19,23 @@
       "    
       '';
   in {
-    systemd.services."unit-status@" = {
-      path = (with pkgs; [ 
-        curl 
-      ]);  
+    systemd.services.
+        systemd.services = {
+          "unit-status@" = {
+            description = "Send status report for failed service %i";
+            path = with pkgs; [ systemd curl ];
 
-      serviceConfig.ExecStart = "${unit_status}/bin/unit_status %I 'Hostname: %H' 'Machine ID: %m' 'Boot ID: %b'";
-      after = [ "network.target" ];
-    };
+            serviceConfig.ExecStart = "${unit_status}/bin/unit_status %I 'Hostname: %H' 'Machine ID: %m' 'Boot ID: %b'";
+            after = [ "network.target" ];
+          };
+
+          # Automatically attach OnFailure=unit-status@%n.service to ALL systemd services
+        } // (lib.mapAttrs (name: service: {
+          # Do not attach the handler to itself or template instances to prevent infinite loops
+          onFailure = lib.mkIf (name != "unit-status@" && !(lib.hasPrefix "unit-status@" name)) [
+            "unit-status@%n.service"
+          ];
+        }) {});
 
     systemd.services."allways-fails" = {
       script = "exit -1";
--- modules/server/wanderer.nix
+++ modules/server/wanderer.nix
@@ -5,7 +5,9 @@
     services.wanderer = {
       enable = true;
       port = 8003;
+      pbPort = 8004;
       origin = "https://wanderer.stroby.org";
+      publicPbUrl = "http://wanderer-db.local";
       dataDir = "/var/lib/wanderer";
       meiliKeySopsField = "meili_master_key";
     };
@@ -14,6 +16,14 @@
       domains = "all";
       root = {
         proxyPass = "http://127.0.0.1:8003";
+        proxyWebsockets = true;    
+      };
+    };
+
+    web_services."wanderer-db" = {
+      domains = "local";
+      root = {
+        proxyPass = "http://127.0.0.1:8004";
         proxyWebsockets = true;    
       };
     };
