ed2277316ea9203b8859a8a0938ecb62b3b05465
Maarten Behn <maarten.behn@gmail.com>
2026-08-14 23:21:45 +0200

    fix

--- modules/server/media/scramjet.nix
+++ modules/server/media/scramjet.nix
@@ -18,7 +18,7 @@
       wispPort = 4142;
       demoHost = "0.0.0.0";
       wispHost = "0.0.0.0";
-      wispUrl = "wss://wsip.local";    
+      wispUrl = "ws://wsip.local";    
     };
 
     systemd.services.scramjet.vpnConfinement = {
