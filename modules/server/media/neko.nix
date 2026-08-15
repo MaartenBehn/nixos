2c872a3b42522016f4d92aa4b67ed04f5c101424
Maarten Behn <maarten.behn@gmail.com>
2026-08-15 02:19:48 +0200

    fix

---
+++ modules/server/media/neko.nix
@@ -0,0 +1,43 @@
+{
+  flake.modules.nixos.server = { config, lib, pkgs, ... }: {
+      virtualisation.oci-containers.containers.neko = {
+        image = "m1k1o/neko:firefox";
+        autoStart = true;
+
+        environment = {
+          NEKO_SCREEN = "1920x1080@30";
+          NEKO_PASSWORD = "changeme-user-password";
+          NEKO_PASSWORD_ADMIN = "changeme-admin-password";
+          NEKO_EPR = "52000-52100"; # WebRTC UDP port range
+          NEKO_ICELITE = "1";
+        };
+
+        ports = [
+          "127.0.0.1:8044:8044"
+          "127.0.0.1:52000-52100:52000-52100/udp"
+        ];
+
+        extraOptions = [
+          "--shm-size=2g"
+          "--cap-add=SYS_ADMIN"
+        ];
+      };
+
+    systemd.services."podman-neko".vpnConfinement = {
+      enable = true;
+      vpnNamespace = "mullvad";
+    };
+
+    vpnNamespaces.mullvad.portMappings = [
+      { from = 8044; to = 8044; }
+    ] ++ (lib.range 52000 52100 |> map (p: { from = p; to = p; protocol = "udp"; }));
+
+    web_services."nenko" = {
+      domains = "local";
+      root = {
+        proxyPass = "http://192.168.15.1:8044/"; 
+        proxyWebsockets = true;
+      };
+    };
+  };
+}
