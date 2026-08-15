e6514268f8416f86070b5c0387bee36b5c4e058f
Maarten Behn <maarten.behn@gmail.com>
2026-08-15 02:39:55 +0200

    fix

--- modules/server/media/neko.nix
+++ modules/server/media/neko.nix
@@ -11,6 +11,7 @@
           NEKO_EPR = "52000-52100"; # WebRTC UDP port range
           NEKO_ICELITE = "1";
           NEKO_SERVER_BIND = ":8044";
+          NEKO_SERVER_PROXY = "true";
         };
 
         extraOptions = [
