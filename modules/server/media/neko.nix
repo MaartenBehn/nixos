6d9ce46ba17865e151e4144a2ea1cf00ad16ce21
Maarten Behn <maarten.behn@gmail.com>
2026-08-15 03:01:32 +0200

    fix

--- modules/server/media/neko.nix
+++ modules/server/media/neko.nix
@@ -6,12 +6,13 @@
 
         environment = {
           NEKO_SCREEN = "1920x1080@30";
-          NEKO_PASSWORD = "changeme-user-password";
-          NEKO_PASSWORD_ADMIN = "changeme-admin-password";
-          NEKO_EPR = "52000-52100"; # WebRTC UDP port range
+          NEKO_PASSWORD = "";
+          NEKO_PASSWORD_ADMIN = "";
+          NEKO_EPR = "52000-52100";
           NEKO_ICELITE = "1";
           NEKO_SERVER_BIND = "192.168.15.1:8044";
           NEKO_SERVER_PROXY = "true";
+          NEKO_WEBRTC_NAT1TO1 = "192.168.15.1";
         };
 
         extraOptions = [
